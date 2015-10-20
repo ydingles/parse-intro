8839e3017d3ceda16c49f483c8596ab47c9d7e4e
Yvonne Ingles <ydingles@hotmail.com>
2015-10-20 10:21:48 -0700

    changes

--- js/music.js
+++ js/music.js
@@ -1,37 +1,66 @@
 // Initialize Parse app
+Parse.initialize("gYHVlOok2M66fC7h22A4ZHSHrY4KD27uge97a2Qb", "t3bGc7lqL1fZI3zQCtvSJV5tdSaGvQMXlX1uzBPq");
 
 
 // Create a new sub-class of the Parse.Object, with name "Music"
+var Music = Parse.Object.extend('Music');
 
 
-// Create a new instance of your Music class 
+// // Create a new instance of your Music class 
+// var sweetTune = new Music();
 
+// // Set a property 'band' equal to a band name
+// sweetTune.set('band', $('#band').val()); 
 
-// Set a property 'band' equal to a band name
-
-
-// Set a property 'website' equal to the band's website
-
+// // Set a property 'website' equal to the band's website
+// sweetTune.set('website', $('#site').val()); 
     
-// Set a property 'song' equal to a song
+// // Set a property 'song' equal to a song
+// sweetTune.set('song', $('#song').val()); 
 
+// // Save your instance of your song -- and go see it on parse.com!
+// sweetTune.save()
 
-// Save your instance of your song -- and go see it on parse.com!
-
+// sweetTune.save(null, {
+//   success: function(music) {
+//     // Execute any logic after the object is saved.
+//     alert('New object created with objectId: ' + music.id);
+//   },
+//   error: function(music, error) {
+//     // Execute any logic that should take place if the save fails.
+//     alert('Failed, with error code: ' + error.message);
+//   }
+// });
 
 // Click event when form is submitted
 $('form').submit(function() {
 
 	// Create a new instance of your Music class 
-
-
 	// For each input element, set a property of your new instance equal to the input's value
-
-
 	// After setting each property, save your new instance back to your database
 
+	// Create a new instance of your Music class 
+	var sweetTune = new Music();
+
+	$(this).find('input').each(function() {
+		instance.set($(this).attr('id'), $(this).val()
+			$(this).val(''))
+	});
 	
-	return false
+	// Set a property 'band' equal to a band name
+	sweetTune.set('band', $('#band').val()); 
+	// Set a property 'website' equal to the band's website
+	sweetTune.set('website', $('#site').val()); 
+ 	// Set a property 'song' equal to a song
+	sweetTune.set('song', $('#song').val()); 
+	// Save your instance of your song -- and go see it on parse.com!
+	sweetTune.save();
+
+	// clear the form
+	$('#band').val(''));
+	$('#site').val(''));
+	$('#song').val(''));
+	return false // prevents a new page from loading
 })
 
 

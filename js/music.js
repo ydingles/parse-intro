8908c39053da5fdacfc96d5b7bfb818efce8eb7c
yvonne ingles <ydingles@hotmail.com>
2015-10-22 10:06:13 -0700

    add

--- js/music.js
+++ js/music.js
@@ -6,32 +6,6 @@ Parse.initialize("gYHVlOok2M66fC7h22A4ZHSHrY4KD27uge97a2Qb", "t3bGc7lqL1fZI3zQCt
 var Music = Parse.Object.extend('Music');
 
 
-// // Create a new instance of your Music class 
-// var sweetTune = new Music();
-
-// // Set a property 'band' equal to a band name
-// sweetTune.set('band', $('#band').val()); 
-
-// // Set a property 'website' equal to the band's website
-// sweetTune.set('website', $('#site').val()); 
-    
-// // Set a property 'song' equal to a song
-// sweetTune.set('song', $('#song').val()); 
-
-// // Save your instance of your song -- and go see it on parse.com!
-// sweetTune.save()
-
-// sweetTune.save(null, {
-//   success: function(music) {
-//     // Execute any logic after the object is saved.
-//     alert('New object created with objectId: ' + music.id);
-//   },
-//   error: function(music, error) {
-//     // Execute any logic that should take place if the save fails.
-//     alert('Failed, with error code: ' + error.message);
-//   }
-// });
-
 // Click event when form is submitted
 $('form').submit(function() {
 
@@ -42,24 +16,30 @@ $('form').submit(function() {
 	// Create a new instance of your Music class 
 	var sweetTune = new Music();
 
-	$(this).find('input').each(function() {
-		instance.set($(this).attr('id'), $(this).val()
-			$(this).val(''))
-	});
+    $(this).find('input').each(function(){
+        sweetTune.set($(this).attr('id'), $(this).val());
+        $(this).val('');
+    });
 	
-	// Set a property 'band' equal to a band name
-	sweetTune.set('band', $('#band').val()); 
-	// Set a property 'website' equal to the band's website
-	sweetTune.set('website', $('#site').val()); 
- 	// Set a property 'song' equal to a song
-	sweetTune.set('song', $('#song').val()); 
-	// Save your instance of your song -- and go see it on parse.com!
-	sweetTune.save();
+    sweetTune.save(null, {
+        success:getData();
+    });
 
-	// clear the form
-	$('#band').val(''));
-	$('#site').val(''));
-	$('#song').val(''));
+ //    or this:
+	// // Set a property 'band' equal to a band name
+	// sweetTune.set('band', $('#band').val()); 
+	// // Set a property 'website' equal to the band's website
+	// sweetTune.set('website', $('#site').val()); 
+ // 	// Set a property 'song' equal to a song
+	// sweetTune.set('song', $('#song').val()); 
+	// // Save your instance of your song -- and go see it on parse.com!
+	// sweetTune.save();
+
+	// // clear the form
+	// $('#band').val('');
+	// $('#site').val('');
+	// $('#song').val('');
+
 	return false // prevents a new page from loading
 })
 
@@ -70,21 +50,36 @@ var getData = function() {
 	
 
 	// Set up a new query for our Music class
-
+    var query = new Parse.Query(Music);
 
 	// Set a parameter for your query -- where the website property isn't missing
-
+    query.notEqualTo('site', '');
 
 	/* Execute the query using ".find".  When successful:
 	    - Pass the returned data into your buildList function
 	*/
+    query.find({
+        success:function(results) {
+            buildList(results);
+        };
+
+        // or success;buildList
+
+    });
+
+
+
+
 }
 
 // A function to build your list
 var buildList = function(data) {
-	// Empty out your unordered list
-	
+	// Empty out your ordered list
+    $('ol').empty();
 	// Loop through your data, and pass each element to the addItem function
+    data.forEach(function(a) {
+        addItem(a);
+    });
 
 }
 
@@ -92,16 +87,27 @@ var buildList = function(data) {
 // This function takes in an item, adds it to the screen
 var addItem = function(item) {
 	// Get parameters (website, band, song) from the data item passed to the function
+    var band = item.get('band');
+    var site = item.get('site');
+    var song = item.song('song');
 
-	
+
 	// Append li that includes text from the data item
-
-
+    var li = $('<li>' + band + ' ' + song + '</li>');
+    var button = $('<button class="btn-danger btn-xs"><span clss="glyphicon glyphicon-remove">Remove</button')
+    button.click(function() {
+        item.destroy({
+            success: getData
+        })
+    })
+    $('ol').append
 	
 	// Time pending, create a button that removes the data item on click
+
+});
 	
 }
 
 // Call your getData function when the page loads
 
-
+getData()
